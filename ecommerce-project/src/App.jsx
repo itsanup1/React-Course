bbdae295ef89473274dd85d267bbb61933c30cad
itsanup1 <96967912+itsanup1@users.noreply.github.com>
2025-09-06 10:44:18 +0530

    testing

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import './App.css';
 import { Routes, Route } from 'react-router';
 
@@ -11,6 +12,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
+      <Route path="trackorders" element={<TrackingPage />} />
     </Routes>
   );
 }
