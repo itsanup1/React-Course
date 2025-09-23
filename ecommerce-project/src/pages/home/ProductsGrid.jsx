ff6f8568389fa068e3a8862966229b6955e6139d
itsanup1 <patilanup7748@gmail.com>
2025-09-23 16:56:41 +0530

    post to cart

--- ecommerce-project/src/pages/home/ProductsGrid.jsx
+++ ecommerce-project/src/pages/home/ProductsGrid.jsx
@@ -1,4 +1,5 @@
 import { formatMoney } from '../../utils/money';
+import axios from 'axios';
 
 export function ProductsGrid({ products }) {
   return (
@@ -49,7 +50,13 @@ export function ProductsGrid({ products }) {
               Added
             </div>
 
-            <button className="add-to-cart-button button-primary">
+            <button className="add-to-cart-button button-primary"
+            onclick = {()=>{
+              axios.post('/api/cart-items', {
+                productId: product.id,
+                quantity:1
+              })
+            }}>
               Add to Cart
             </button>
           </div>
